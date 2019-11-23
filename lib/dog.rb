b851c25c532b2408fae7b17e35d27b0478492b03
vy vo <tuonggvy.vo@gmail.com>
2019-11-23 04:57:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  
+  def name
+    @this_dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
